d7db67effc6425915ffaa1728b83a26a25fa69f5
zhangzian <zhangzian@kevin-joe-2.3521.dhcp.asu.edu>
2019-02-25 12:42:59 -0700

    Initial check in

---
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,68 @@
+//Name: Zian Zhang
+//Class ID: 596
+package cse360assign2;
+
+/**
+ * Description: This class is used to do basic calculation.
+ * @author zhangzian
+ *
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Description: this is class constructor
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Description: This method get the total sum
+	 * @return an integer
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Description: This method is calculate summation 
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Description: This method is calculate subtraction
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Description: THis method is calculate multiplication
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Description: This method is calculate division 
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Description: This method get the history
+	 * @return a String
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
